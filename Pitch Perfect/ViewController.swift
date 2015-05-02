86636261edaf83ab70fb48aed62a9a6e96baf18b
Jake Romer <jkrmr@users.noreply.github.com>
2015-05-02 08:19:07 -0700

    Add audio recording start / stop logic

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -1,18 +1,20 @@
-//
 //  ViewController.swift
 //  Pitch Perfect
 //
 //  Created by Jake Romer on 5/2/15.
 //  Copyright (c) 2015 jkrmr.io. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, 
+        // typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        recordingInProgress.hidden = false
+        println("recording Audio");
+    }
 
+    @IBAction func stopRecording(sender: UIButton) {
+        recordingInProgress.hidden = true
+        println("recording stopped");
+    }
 }
 
